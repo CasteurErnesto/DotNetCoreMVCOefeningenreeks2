bb5da5e9e1a68d132e7b3ca8a8eafce41b27c308
Olivier Sourie <Olivier.Sourie@howest.be>
2018-03-08 21:10:43 +0100

    Controller with List and Create

---
+++ DotNetCoreMVCOefeningenreeks2/Controllers/ShoppingController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotNetCoreMVCOefeningenreeks2.Entities;
+
+namespace DotNetCoreMVCOefeningenreeks2.Controllers
+{
+    //Scaffold-DbContext -Connection "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MyShopLi;Integrated Security=True" -Provider Microsoft.EntityFrameworkCore.SqlServer -OutputDir Entities -Context MyShopLiContext
+
+    public class ShoppingController : Controller
+    {
+        MyShopLiContext db;
+        public ShoppingController()
+        {
+            db = new MyShopLiContext();
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.ShopItem.ToList());
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View(new ShopItem());
+        }
+
+        [HttpPost]
+        public IActionResult Create(ShopItem shopItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ShopItem.Add(shopItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(shopItem);
+            }
+        }
+
+        public ViewResult Filter(string keyword)
+        {
+            return View();
+            //return View("Index", db.Maaltijd.FromSql($"Select * from Maaltijd  where Type = {keyword} ").ToList());
+        }
+    }
+}
---
+++ DotNetCoreMVCOefeningenreeks2/Entities/ShopItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCoreMVCOefeningenreeks2.Entities
+{
+    public partial class ShopItem
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public byte Quantity { get; set; }
+    }
+}
